36f79021976f38a1bd588a4fc8e35bcd58c2347b
Rahul Raj P <raj431099@gmail.com>
2026-03-21 16:35:07 +0530

    finished the appbar in homescreen

--- lib/presentation/home/homescreen.dart
+++ lib/presentation/home/homescreen.dart
@@ -10,9 +10,30 @@ class Homescreen extends StatelessWidget {
         backgroundColor: Color(0xff7C2F02),
         elevation: 0,
         leading: Padding(
-          padding: EdgeInsets.all(8),
-          child: Image.asset('assets/images/alisoninfotechappbarlogo.png'),
+          padding: EdgeInsets.all(4),
+          child: Image.asset(
+            'assets/images/alisoninfotechappbarlogo.png',
+            width: 69,
+            height: 42,
+          ),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search, color: Colors.white),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite_border, color: Colors.white),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.notifications_active_outlined,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
